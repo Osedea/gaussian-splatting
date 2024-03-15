188c2867723c8551e1542321ae846bd59186c3cf
Isabelle Bouchard <isabelle.bouchard@osedea.com>
2024-03-15 11:18:53 -0400

    remove network gui

--- gaussian_splatting/training.py
+++ gaussian_splatting/training.py
@@ -2,7 +2,7 @@ import os
 import torch
 from random import randint
 from gaussian_splatting.utils.loss_utils import l1_loss, ssim
-from gaussian_splatting.gaussian_renderer import render, network_gui
+from gaussian_splatting.gaussian_renderer import render
 import sys
 from gaussian_splatting.scene import Scene, GaussianModel
 from gaussian_splatting.utils.general_utils import safe_state
@@ -11,12 +11,6 @@ from tqdm import tqdm
 from gaussian_splatting.utils.image_utils import psnr
 from argparse import Namespace
 from gaussian_splatting.arguments import ModelParams, PipelineParams, OptimizationParams
-
-try:
-    from torch.utils.tensorboard import SummaryWriter
-    TENSORBOARD_FOUND = True
-except ImportError:
-    TENSORBOARD_FOUND = False
 
 
 class Trainer:
@@ -28,8 +22,6 @@ class Trainer:
         checkpoint=None,
         debug_from=-1,
         quiet=False,
-        ip="127.0.0.1",
-        port=6009,
         detect_anomaly=False
     ):
         if testing_iterations is None:
@@ -47,10 +39,8 @@ class Trainer:
         self._checkpoint = checkpoint
         self._debug_from = debug_from
 
-        # Initialize system state (RNG)
         safe_state(quiet)
-        # Start GUI server, configure and run training
-        network_gui.init(ip, port)
+
         torch.autograd.set_detect_anomaly(detect_anomaly)
 
 
@@ -61,7 +51,6 @@ class Trainer:
         pipe,
    ):
         first_iter = 0
-        tb_writer = prepare_output_and_logger(dataset)
         gaussians = GaussianModel(dataset.sh_degree)
         scene = Scene(dataset, gaussians)
         gaussians.training_setup(opt)
@@ -80,21 +69,6 @@ class Trainer:
         progress_bar = tqdm(range(first_iter, opt.iterations), desc="Training progress")
         first_iter += 1
         for iteration in range(first_iter, opt.iterations + 1):
-            if network_gui.conn == None:
-                network_gui.try_connect()
-            while network_gui.conn != None:
-                try:
-                    net_image_bytes = None
-                    custom_cam, do_training, pipe.convert_SHs_python, pipe.compute_cov3D_python, keep_alive, scaling_modifer = network_gui.receive()
-                    if custom_cam != None:
-                        net_image = render(custom_cam, gaussians, pipe, background, scaling_modifer)["render"]
-                        net_image_bytes = memoryview((torch.clamp(net_image, min=0, max=1.0) * 255).byte().permute(1, 2, 0).contiguous().cpu().numpy())
-                    network_gui.send(net_image_bytes, dataset.source_path)
-                    if do_training and ((iteration < int(opt.iterations)) or not keep_alive):
-                        break
-                except Exception as e:
-                    network_gui.conn = None
-
             iter_start.record()
 
             gaussians.update_learning_rate(iteration)
@@ -136,7 +110,6 @@ class Trainer:
 
                 # Log and save
                 training_report(
-                    tb_writer,
                     iteration,
                     Ll1,
                     loss,
@@ -188,21 +161,8 @@ def prepare_output_and_logger(args):
     with open(os.path.join(args.model_path, "cfg_args"), 'w') as cfg_log_f:
         cfg_log_f.write(str(Namespace(**vars(args))))
 
-    # Create Tensorboard writer
-    tb_writer = None
-    if TENSORBOARD_FOUND:
-        tb_writer = SummaryWriter(args.model_path)
-    else:
-        print("Tensorboard not available: not logging progress")
-    return tb_writer
 
-
-def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_iterations, scene : Scene, renderFunc, renderArgs):
-    if tb_writer:
-        tb_writer.add_scalar('train_loss_patches/l1_loss', Ll1.item(), iteration)
-        tb_writer.add_scalar('train_loss_patches/total_loss', loss.item(), iteration)
-        tb_writer.add_scalar('iter_time', elapsed, iteration)
-
+def training_report(iteration, Ll1, loss, l1_loss, elapsed, testing_iterations, scene : Scene, renderFunc, renderArgs):
     # Report test and samples of training set
     if iteration in testing_iterations:
         torch.cuda.empty_cache()
@@ -216,22 +176,12 @@ def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_i
                 for idx, viewpoint in enumerate(config['cameras']):
                     image = torch.clamp(renderFunc(viewpoint, scene.gaussians, *renderArgs)["render"], 0.0, 1.0)
                     gt_image = torch.clamp(viewpoint.original_image.to("cuda"), 0.0, 1.0)
-                    if tb_writer and (idx < 5):
-                        tb_writer.add_images(config['name'] + "_view_{}/render".format(viewpoint.image_name), image[None], global_step=iteration)
-                        if iteration == testing_iterations[0]:
-                            tb_writer.add_images(config['name'] + "_view_{}/ground_truth".format(viewpoint.image_name), gt_image[None], global_step=iteration)
                     l1_test += l1_loss(image, gt_image).mean().double()
                     psnr_test += psnr(image, gt_image).mean().double()
                 psnr_test /= len(config['cameras'])
                 l1_test /= len(config['cameras'])
                 print("\n[ITER {}] Evaluating {}: L1 {} PSNR {}".format(iteration, config['name'], l1_test, psnr_test))
-                if tb_writer:
-                    tb_writer.add_scalar(config['name'] + '/loss_viewpoint - l1_loss', l1_test, iteration)
-                    tb_writer.add_scalar(config['name'] + '/loss_viewpoint - psnr', psnr_test, iteration)
 
-        if tb_writer:
-            tb_writer.add_histogram("scene/opacity_histogram", scene.gaussians.get_opacity, iteration)
-            tb_writer.add_scalar('total_points', scene.gaussians.get_xyz.shape[0], iteration)
         torch.cuda.empty_cache()
 
     print("\nTraining complete.")
